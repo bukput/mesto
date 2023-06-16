0985458cbcc65c2a956c2c0098745a65309822c7
Ivan Mitrofanov <mim300104@icloud.com>
2023-06-16 07:14:00 +0300

    zest

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,16 +3,14 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupElement) {
     super(popupElement);
-
     this._title = this._popup.querySelector('.popup-fullScreen__caption');
     this._image = this._popup.querySelector('.popup-fullScreen__img');
-    this._alt = this._image;
   }
 
-  open(name, link, alt) {
+  open(name, link) {
     super.open();
     this._title.textContent = name;
     this._image.src = link;
-    this._alt.alt = alt;
+    this._image.alt = name;
   }
 }
